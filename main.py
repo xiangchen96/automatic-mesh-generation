3b8a510b9b6a8411171eb30682e242361e230bf4
Xiang <xiang1016127637@gmail.com>
2018-06-06 12:07:50 +0200

    Puntos2

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import dcel
 import numpy as np
-import triangle
-import triangle.plot as plot
+#import triangle
+#import triangle.plot as plot
 import matplotlib.pyplot as plt
 
-#fileName = input("Introduzca el nombre del archivo de puntos:")
-fileName = "puntos2"
+fileName = input("Introduzca el nombre del archivo de puntos:")
+#fileName = "puntos2"
 
 D = dcel.Dcel.deloneFromPolygonFile(fileName)
 f = open(fileName,"r")
@@ -15,7 +15,8 @@ for i in range(number_of_points):
     line = f.readline()
     x,y = line.split(" ")
     puntos.append([float(x),float(y)])
-    
+f.close()
+
 #alpha = int(input("Introduzca el alpha (angulo minimo):"))
 alpha = 20
 
@@ -23,16 +24,16 @@ D.alpha = alpha
 
 print("\nPoligono inicial con angulo minimo %d:\n"%D.get_minimun_angle())
 D.plotPolygon()
-points_and_segments = {'vertices':np.array(puntos), 
-                       'segments':np.array([[i,(i+1)%len(puntos)] for i in range(len(puntos))])} 
+#points_and_segments = {'vertices':np.array(puntos), 
+#                       'segments':np.array([[i,(i+1)%len(puntos)] for i in range(len(puntos))])} 
 
 
-triangulation = triangle.triangulate(points_and_segments, 'pq'+str(alpha))
+#triangulation = triangle.triangulate(points_and_segments, 'pq'+str(alpha))
 
-ax = plt.axes()
-plot.plot(ax, **triangulation)
-plt.show()
-print("vertices:",len(triangulation['vertices']))
+#ax = plt.axes()
+#plot.plot(ax, **triangulation)
+#plt.show()
+#print("vertices:",len(triangulation['vertices']))
 
 #D.animate_main()
 
